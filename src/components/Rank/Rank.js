2bc36b20297c735735eb0ed9d2ef891e29517607
olubanwo Ayomide <olubanwoayomide@gmail.com>
2020-11-11 18:13:48 +0100

    Rank component

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react' ;
+
+const Rank = () => {
+	return (
+		<div className=''>
+			<div className= 'white f3 center'>
+				Your current rank is...
+			</div>
+			<div className='white f2 center'>
+				#2
+			</div>
+		</div>
+	);
+}
+
+export default Rank; 
