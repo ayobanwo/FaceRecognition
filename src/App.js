c6f6ea919c0ea81a029ed8c70d543c55bc4489d4
olubanwo Ayomide <olubanwoayomide@gmail.com>
2020-11-12 12:19:04 +0100

    Particle css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+import Navigation from './components/Navigation';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Logo from './components/logo/Logo';
+import Rank from './components/Rank/Rank';
 import './App.css';
 
-class App extends Component {
-      return (
-        <div className="App">
-            <Navigation /> 
-        </div>
-      )
+const particlesOptions = {
+	particles: {
+		number: {
+			value: 60,
+			density: {
+				enable: true,
+				value_area: 500
+			}
+		}
+	}
+}
+
+class App extends Component{
+	render(){
+	    return (
+	        <div className="App">
+	        	<Particles className='particles'
+	        	    params={particlesOptions}
+	        	 />
+	            <Navigation /> 
+	      		<Logo />
+	      		<Rank />
+	            <ImageLinkForm />
+	                       {  /*<FaceRecognition />*/}
+	        </div>
+	    );
+	}
 }
 
 export default App;
