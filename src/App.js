6d9d231e0cf1af986641cad819d84efcc0007cf6
olubanwo Ayomide <olubanwoayomide@gmail.com>
2020-11-12 15:43:15 +0100

    Image Recognition API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Logo from './components/logo/Logo';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition'
 import './App.css';
+
+const app = new Clarifai.App({
+ apiKey: '07a6f5135ec9445aa30af05bac535607'
+});
 
 const particlesOptions = {
 	particles: {
@@ -19,6 +25,33 @@ const particlesOptions = {
 }
 
 class App extends Component{
+	constructor() {
+		super();
+		this.state = {
+			input: '',
+			imageUrl: ''
+		}
+	}
+
+	onInputChange = (event) => {
+		this.setState({input: event.target.value});
+	} 
+
+	onSubmit = () => {
+		this.setState({imageUrl: this.state.input})
+		console.log('click')
+		app.models.predict(
+			Clarifai.FACE_DETECT_MODEL, 
+			this.state.input ).then(
+			function(response) {
+				console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+			},
+			function(err) {
+
+			} 
+		);
+	} 
+
 	render(){
 	    return (
 	        <div className="App">
@@ -28,8 +61,8 @@ class App extends Component{
 	            <Navigation /> 
 	      		<Logo />
 	      		<Rank />
-	            <ImageLinkForm />
-	                       {  /*<FaceRecognition />*/}
+	            <ImageLinkForm onInputChange = {this.onInputChange} onSubmit ={this.onSubmit} />
+	            <FaceRecognition imageUrl={this.state.imageUrl} />
 	        </div>
 	    );
 	}
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,16 +1,16 @@
 import React from 'react' ;
 import  './ImageLinkForm.css';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onSubmit }) => {
 	return (
 		<div>
 			<p className='center f3'>
 				Detects faces in pictures
 			</p>
-			<div className='center'>
+			<div className='center' >
 				<div className= 'fback form pa4 br3 shadow-5 center'>
-					<input className='f4 pa2 w-70 center' type="text"/>
-					<button className='w-30 grow f4 link ph3 pv2 dib white bg-blue br2'> Detect </button>
+					<input className='f4 pa2 w-70 center' type="text" onChange ={ onInputChange } />
+					<button className='w-30 grow f4 link ph3 pv2 dib white bg-blue br2' onClick ={onSubmit} > Detect </button>
 				</div>
 			</div>
 		</div>
