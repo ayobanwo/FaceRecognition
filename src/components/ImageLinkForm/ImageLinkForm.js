4db78f12f4dc74382d24168879022c2ead885350
olubanwo Ayomide <olubanwoayomide@gmail.com>
2020-11-16 12:02:57 +0100

    CSS Responsiveness

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -3,13 +3,13 @@ import  './ImageLinkForm.css';
 
 const ImageLinkForm = ({ onInputChange, onSubmit }) => {
 	return (
-		<div>
+		<div className = 'main'>
 			<p className='center f3'>
-				Detects faces in pictures
+				Input Image to Detect Faces
 			</p>
 			<div className='center' >
 				<div className= 'fback form pa4 br3 shadow-5 center'>
-					<input className='f4 pa2 w-70 center' type="text" onChange ={ onInputChange } />
+					<input className='f4 pa2 w-70 center hover-bg-black hover-white' type="text" onChange ={ onInputChange } />
 					<button className='w-30 grow f4 link ph3 pv2 dib white bg-blue br2' onClick ={onSubmit} > Detect </button>
 				</div>
 			</div>
